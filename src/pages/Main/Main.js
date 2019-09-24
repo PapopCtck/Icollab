6cb56d86f316fa3585b3381364c5b9cd9104d2c4
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-09-24 22:14:12 +0700

    [Update] Update Main.js to reder NavBar

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react'
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
+
+import { NavBar } from '../../helpers';
+
+import './StyleMain.css';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 export class Main extends Component {
   render() {
     return (
-      <Layout className="layout">
+      <Layout className="layout" style={{ height: '100vh', width: '100vw' }}>
+        <NavBar></NavBar>
         <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
         </Header>
         <Layout>
-          <Sider></Sider>
-          <Content style={{ padding: '0 50px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          <Sider className="sider"/>
+          <Content style={{ padding: '0 50px', height: '200px' }}>
+            <div className="regular">
+              this is regular
+            </div>
+            <div className="bold">
+              this is bold
+            </div>
           </Content>
-          <Sider></Sider>
+          <Sider className="sider" />
         </Layout>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
