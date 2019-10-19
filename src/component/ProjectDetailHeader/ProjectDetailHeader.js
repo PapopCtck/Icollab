bdb7bce8152eea8ab5f429462c03b77f368da419
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-10-19 19:36:43 +0700

    [Update] Update ProjectDetailHeader to render from data

--- src/component/ProjectDetailHeader/ProjectDetailHeader.js
+++ src/component/ProjectDetailHeader/ProjectDetailHeader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { Typography, Button, Icon } from 'antd';
 
 import './StyleProjectDetailHeader.css';
 
@@ -7,48 +8,47 @@ const { Title } = Typography;
 
 export class DetailHeader extends Component {
   render() {
-    const { projectDetail } = this.props;
+    const { projectDetail, projectId } = this.props;
     return (
       <div className="detailheader-carousel-container">
         <div className="detailheader-image-container">
           <img className="detailheader-image" src={projectDetail.projectThumbnail} alt={projectDetail.projectTitle} />
         </div>
-        <div className="carousel-content-container detailcarousel-content-container">
+        <div className="carousel-content-container detailheader-carousel-content-container">
           <div className="carousel-content-title">
             <Title level={2}>
-              Header
+              {projectDetail.projectTitle + ' ' + projectId}
             </Title>
           </div>
-          <div className="carousel-content-sub-title">
-            <span className="carousel-content-static">
-              level :
-            </span>
-            <span className="carousel-content-level">
-              123
-            </span>
-            <span className="carousel-content-static">
-              role needed :
-            </span>
-            <span className="carousel-content-role">
-              idk
-            </span>
-          </div>
-          <div className="carousel-content-description">
-            &quot;
-          desc
-            &quot;
-          </div>
-          <div className="carousel-content-starter-container">
-            <Avatar size={50} className="carousel-content-starter-avatar" icon="user"></Avatar>
-            <div className="carousel-content-starter-detail">
-              <div className="carousel-content-starter-name">
-                full name
-              </div>
-              <div className="crousel-content-starter-assoc">
-                asssoc
-              </div>
+          <div className="carousel-content-sub-title detailheader-carousel-content-sub-title">
+            <div className="carousel-content-level detailheader-carousel-content">
+              <span className="bold">level : </span>
+              {projectDetail.projectLevel.map((level, idx) => idx === 0 ? level : ', ' + level)}
             </div>
-            <div>
+            <div className="carousel-content-role detailheader-carousel-content">
+              <span className="bold">role needed : </span>
+              {projectDetail.roleNeeded.map((role, idx) => idx === 0 ? role : ', ' + role)}
+            </div>
+            <div className="carousel-content-industry detailheader-carousel-content">
+              <span className="bold">Industry : </span>
+              {projectDetail.projectIndustry.map((ind, idx) => idx === 0 ? ind : ', ' + ind)}
+            </div>
+            <div className="carousel-content-location detailheader-carousel-content">
+              <span className="bold">location : </span>
+              {projectDetail.projectLocation}
+            </div>
+            <Button className="detailheader-carousel-applybtn" type="primary" block>
+              Apply now
+            </Button>
+            <div className="detailheader-carousel-contact-container">
+              <Button className="detailheader-carousel-chatbtn" icon="mail" type="primary" ghost>talk with us!</Button>
+              <Icon type="facebook" className="primary-icon social-icon" />
+              <Icon type="instagram" className="primary-icon social-icon" />
+              <Icon type="twitter" className="primary-icon social-icon" />
+              <Icon type="reddit" className="primary-icon social-icon" />
+            </div>
+            <div className="detailheader-carousel-report-container">
+              think this project should be at another level ? <Link to="#">tell us</Link>
             </div>
           </div>
         </div>
