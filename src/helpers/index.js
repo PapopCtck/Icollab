1a4014a6b754510bb41953bd0e53a477b62e385b
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-11-08 23:29:56 +0700

    [Update] Export function redirect and loading

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,9 +2,17 @@ import NavBar from './NavBar/NavBar';
 
 import CarouselDisplay from './CarouselDisplay/CarouselDisplay';
 
+import FNRedirect from './Functions/FNRedirect';
+
+import Loading from './Loading/Loading';
+
 export {
   //NavBar
   NavBar,
   //Carousel
   CarouselDisplay,
+  //Function
+  FNRedirect,
+  //Loading
+  Loading,
 }
