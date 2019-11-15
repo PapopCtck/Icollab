191874a94339dc5926c4db94715eed571b6450a6
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-11-15 21:18:53 +0700

    [Create] Create ExploreTResult component

---
+++ src/component/ExploreResult/ExploreResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ExploreResult extends Component {
+  render() {
+    return (
+      <div>
+        Result
+      </div>
+    )
+  }
+}
+
+export default ExploreResult
