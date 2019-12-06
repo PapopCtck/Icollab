0c7bac5179d1c8de578804ba20b42c294d58c666
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-12-06 20:03:01 +0700

    [Update] Update projectDetail to render from fetchProjectById

--- src/component/ProjectDetailStory/ProjectDetailStory.js
+++ src/component/ProjectDetailStory/ProjectDetailStory.js
@@ -8,20 +8,20 @@ const { Meta } = Card;
 
 export class ProjectDetailStory extends Component {
   render() {
-    const { projectDetail } = this.props;
+    const { projectDetail, mockupData } = this.props;
     return (
       <div className="projectdetail-story-container">
         <div className="projectdetail-story-main">
           <h3>
             Quisque viverra interdum velit?
           </h3>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit suscipit, condimentum mauris convallis cras iaculis at tempus dignissim arcu, sollicitudin posuere porta non tellus dictumst penatibus. Commodo ante quis senectus quisque semper dapibus porttitor libero lacus hendrerit volutpat, in cras a himenaeos leo ridiculus taciti ultrices cursus mattis. Tempor mi fames senectus taciti condimentum lacinia ante, non netus rhoncus eget habitant sollicitudin, cubilia luctus consequat pharetra natoque porta. Sodales tristique habitasse massa ultrices ligula semper interdum dictumst, at quis vel non facilisis congue cursus neque mollis, pretium consequat suspendisse imperdiet volutpat sollicitudin luctus.
+          {projectDetail.projectdescription}
         </div>
         <div className="projectdetail-story-sider">
           <div className="projectdetail-story-roleneeded">
             <h3 className="bold">Who are we looking for ?</h3>
             {
-              projectDetail.roleNeeded.map((role) =>
+              mockupData.roleNeeded.map((role) =>
                 <Card className="projectdetail-story-rolecard-container">
                   <Meta className="bold" title={role.title} />
                   <div className="projectdetail-story-rolecard-skill">
@@ -39,7 +39,7 @@ export class ProjectDetailStory extends Component {
           </div>
           <div className="projectdetail-story-staters">
             <h3 className="bold">About us</h3>
-            {projectDetail.projectStarters.map((starter) =>
+            {mockupData.projectStarters.map((starter) =>
               <Card className="projectdetail-story-statercard-container">
                 <div className="projectdetail-story-statercard">
                   <Avatar size="large" className="projectdetail-story-statercard-avatar" src={starter.userImg} />
