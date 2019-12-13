48b3972ffe0dc77c1b8c372dbb9990de953d086a
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-12-13 19:34:34 +0700

    [Update] Update sort button and sort function fro explore page

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -17,9 +17,12 @@ export class Explore extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps.fetchGetProjects !== this.props.fetchGetProjects) {
       const fetchGetProjects = this.props.fetchGetProjects;
-      this.setState({ resultProjects: fetchGetProjects.Project },() => console.log(this.state));
+      const resultProjects = fetchGetProjects.Project.sort((a, b) => this.dateSort(a.createat, b.createat))
+      this.setState({ resultProjects: resultProjects }, () => console.log(this.state));
     }
   }
+
+  dateSort = (firstDate, secondDate) => new Date(secondDate) - new Date(firstDate);
 
   handleChange = (text) => {
     console.log(text);
@@ -33,12 +36,36 @@ export class Explore extends Component {
     console.log(`checked = ${e.target.checked}`);
   }
 
+  handleSortSelect = (name) => {
+    const { resultProjects } = this.state;
+    let sortedProject = null;
+    switch (name) {
+      case 'Trending':
+        console.log('sort by trending')
+        sortedProject = resultProjects.sort((a, b) => b.view - a.view)
+        this.setState({ resultProjects : sortedProject })
+        break;
+      case 'DateAdded':
+        console.log('sort by DateAdded')
+        sortedProject = resultProjects.sort((a, b) => this.dateSort(a.createat, b.createat))
+        this.setState({ resultProjects : sortedProject })
+        break;
+      case 'Update':
+        console.log('sort by Update')
+        sortedProject = resultProjects.sort((a, b) => this.dateSort(a.lastupdate, b.lastupdate))
+        this.setState({ resultProjects : sortedProject })
+        break;
+      default:
+        break;
+    }
+  }
+
   render() {
     const { resultProjects } = this.state;
     return (
       <div>
         <ExploreCondition handleChange={this.handleChange} handleSearch={this.handleSearch} />
-        <ExploreResult resultProjects={resultProjects} handleChange={this.handleChange} handleCheck={this.handleCheck} />
+        <ExploreResult resultProjects={resultProjects} handleChange={this.handleChange} handleCheck={this.handleCheck} handleSortSelect={this.handleSortSelect} />
       </div>
     )
   }
