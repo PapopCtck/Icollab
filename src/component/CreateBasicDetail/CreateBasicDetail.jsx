4ecf1ea878d2a4c1e9b60cf98c410670b3544037
PapopCtck <dpprachaya@gmail.com>
2019-12-25 00:50:31 +0700

    [Update] Update Layout of createBasicDetail

--- src/component/CreateBasicDetail/CreateBasicDetail.jsx
+++ src/component/CreateBasicDetail/CreateBasicDetail.jsx
@@ -1,86 +1,99 @@
 import React, { Component } from 'react';
-import { Select, Button, Typography } from 'antd';
+import { Select, Button, Typography, Layout } from 'antd';
 import QueueAnim from 'rc-queue-anim';
+
+import { MainNav } from '../../component';
 
 import './StyleCreateBasicDetail.css';
 
 const { Title } = Typography;
 const { Option } = Select;
+const { Header, Content } = Layout;
 
 export class CreateBasicDetail extends Component {
   render() {
-    const { onFinishBasic, show } = this.props;
+    const { onFinishBasic, show, onSelect } = this.props;
     return (
-      <div className="create-basic-container">
-        <QueueAnim type={['bottom', 'top']} >
-          {show ? [
-            <QueueAnim key="page" type="bottom">
-              <div className="create-basic-header-container">
-                <Title level={2} className="login-title">First, let&apos;s fill in the basic detail</Title>
-                <p className="create-basic-subheader" key="p" >
-                  we need these detail to match you up with appropriate community.You can always update this later.
-                </p>
-              </div>
-              <div className="create-basic-select-container">
-                <QueueAnim type="bottom" >
-                  <div className="create-basic-select">
-                    <h4 className="create-basic-select-label">project category</h4>
-                    <Select
-                      style={{ width: 200 }}
-                      placeholder="Select a person"
-                      optionFilterProp="children"
-                      onChange={this.onChange}
-                      filterOption={(input, option) =>
-                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                      }
-                    >
-                      <Option value="jack">Jack</Option>
-                      <Option value="lucy">Lucy</Option>
-                      <Option value="tom">Tom</Option>
-                    </Select>
-                  </div>
-                  <div className="create-basic-select">
-                    <h4 className="create-basic-select-label">location</h4>
-                    <Select
-                      style={{ width: 200 }}
-                      placeholder="Select a person"
-                      optionFilterProp="children"
-                      onChange={this.onChange}
-                      filterOption={(input, option) =>
-                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                      }
-                    >
-                      <Option value="jack">Jack</Option>
-                      <Option value="lucy">Lucy</Option>
-                      <Option value="tom">Tom</Option>
-                    </Select>
-                  </div>
-                  <div className="create-basic-select">
-                    <h4 className="create-basic-select-label">skill level</h4>
-                    <Select
-                      style={{ width: 200 }}
-                      placeholder="Select a person"
-                      optionFilterProp="children"
-                      onChange={this.onChange}
-                      filterOption={(input, option) =>
-                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                      }
-                    >
-                      <Option value="jack">Jack</Option>
-                      <Option value="lucy">Lucy</Option>
-                      <Option value="tom">Tom</Option>
-                    </Select>
-                  </div>
-                  <div className="create-basic-select-button">
-                    <Button type="primary" onClick={onFinishBasic}>Finish</Button>
-                  </div>
-                </QueueAnim>
-              </div>
 
-            </QueueAnim>,
-          ] : null}
-        </QueueAnim>
-      </div>
+      <QueueAnim className="create-basic" delay={500} type={['bottom', 'top']} ease={['easeOutQuart', 'easeInOutQuart']}>
+        {show ? [
+          <QueueAnim key="page-container" type="bottom">
+            <Layout className="create-basic-layout">
+              <Header className="header-container">
+                <MainNav />
+              </Header>
+              <Content>
+                <div className="create-basic-container">
+                  <QueueAnim key="page" type="bottom">
+                    <div className="create-basic-header-container">
+                      <Title level={2} className="create-title">First, let&apos;s fill in the basic detail</Title>
+                      <p className="create-basic-subheader" key="p" >
+                        we need these detail to match you up with appropriate community.You can always update this later.
+                      </p>
+                    </div>
+                    <div className="create-basic-select-container">
+                      <QueueAnim type="bottom" >
+                        <div className="create-basic-select">
+                          <h4 className="create-basic-select-label">project category</h4>
+                          <Select
+                            style={{ width: 200 }}
+                            placeholder="Select a person"
+                            optionFilterProp="children"
+                            onChange={(value) => onSelect(value, 'category')}
+                            filterOption={(input, option) =>
+                              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                            }
+                          >
+                            <Option value="jack">Jack</Option>
+                            <Option value="lucy">Lucy</Option>
+                            <Option value="tom">Tom</Option>
+                          </Select>
+                        </div>
+                        <div className="create-basic-select">
+                          <h4 className="create-basic-select-label">location</h4>
+                          <Select
+                            style={{ width: 200 }}
+                            placeholder="Select a person"
+                            optionFilterProp="children"
+                            onChange={(value) => onSelect(value, 'location')}
+                            filterOption={(input, option) =>
+                              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                            }
+                          >
+                            <Option value="jack">Jack</Option>
+                            <Option value="lucy">Lucy</Option>
+                            <Option value="tom">Tom</Option>
+                          </Select>
+                        </div>
+                        <div className="create-basic-select">
+                          <h4 className="create-basic-select-label">skill level</h4>
+                          <Select
+                            style={{ width: 200 }}
+                            placeholder="Select a person"
+                            optionFilterProp="children"
+                            onChange={(value) => onSelect(value, 'skill')}
+                            filterOption={(input, option) =>
+                              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                            }
+                          >
+                            <Option value="jack">Jack</Option>
+                            <Option value="lucy">Lucy</Option>
+                            <Option value="tom">Tom</Option>
+                          </Select>
+                        </div>
+                        <div className="create-basic-select-button">
+                          <Button type="primary" onClick={onFinishBasic}>Finish</Button>
+                        </div>
+                      </QueueAnim>
+                    </div>
+                  </QueueAnim>
+                </div>
+              </Content>
+            </Layout>
+          </QueueAnim>,
+        ] : null
+        }
+      </QueueAnim>
     )
   }
 }
