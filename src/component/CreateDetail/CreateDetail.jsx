2e82080a8fbcc00a5a2149262fe5854c3d113eae
PapopCtck <dpprachaya@gmail.com>
2019-12-25 18:47:57 +0700

    [Update] Pass header and onFinish props to tab helper

--- src/component/CreateDetail/CreateDetail.jsx
+++ src/component/CreateDetail/CreateDetail.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Input, Typography, Select } from 'antd';
 import QueueAnim from 'rc-queue-anim';
-
+import PropTypes from 'prop-types';
 
 import {
   TabsBar,
@@ -15,34 +15,39 @@ const { Title } = Typography;
 
 export class CreateDetail extends Component {
   render() {
-    const { show, onInput, onSelect, imageUrl, onImageUpload, setImage } = this.props;
+    const { show, onInput, onSelect, imageUrl, onImageUpload, setImage, onFinish } = this.props;
     return (
       <div className="create-detail-container">
         <QueueAnim className="create-basic" delay={1000} type={['bottom', 'top']} ease={['easeOutQuart', 'easeInOutQuart']}>
           {!show ? [
             <QueueAnim key="page" type="bottom">
-              <TabsBar>
-                <Tab label="Basic" >
-                  <Title level={4} className="create-title">Basic information</Title>
+              <TabsBar onFinish={onFinish}>
+                <Tab label="Basic" header="Basic information" >
+                  <Title level={4} className="create-title">Basics</Title>
                   <p className="create-description">
                     Make a good first impression: introduce your project objectives and entice people to learn more.
                       This basic information will represent your project on your project page, on your project card,
                       and in searches.</p>
-                  <CreateDetailBasic onInput={onInput} onSelect={onSelect} imageUrl={imageUrl} onImageUpload={onImageUpload} setImage={setImage} />
+                  <CreateDetailBasic
+                    onInput={onInput}
+                    onSelect={onSelect}
+                    imageUrl={imageUrl}
+                    onImageUpload={onImageUpload}
+                    setImage={setImage} />
                 </Tab>
-                <Tab label="Story">
+                <Tab label="Story" header="Project Story">
                   <Title level={4} className="create-title">Story</Title>
                   <p className="create-description">
-                   Tell people why they should be excited about your project. Get specific but be clear and be brief..</p>
+                    Tell people why they should be excited about your project. Get specific but be clear and be brief..</p>
                   <CreateDetailBasic onInput={onInput} onSelect={onSelect} imageUrl={imageUrl} onImageUpload={onImageUpload} setImage={setImage} />
                 </Tab>
-                <Tab label="FAQ">
+                <Tab label="FAQ" header="Frequently Ask Question">
                   test3
                 </Tab>
-                <Tab label="People">
+                <Tab label="People" header="Add People">
                   test4
                 </Tab>
-                <Tab label="Extras">
+                <Tab label="Extras" header="Extras">
                   test5
                 </Tab>
               </TabsBar>
@@ -57,17 +62,27 @@ export class CreateDetail extends Component {
 
 export default CreateDetail;
 
+CreateDetail.propTypes = {
+  show: PropTypes.bool,
+  onInput: PropTypes.func,
+  onSelect: PropTypes.func,
+  imageUrl: PropTypes.string,
+  onImageUpload: PropTypes.func,
+  setImage: PropTypes.func,
+  onFinish: PropTypes.func,
+}
+
 export const CreateDetailBasic = ({ onInput, onSelect, imageUrl, onImageUpload, setImage }) => (
   <div className="create-detail-basic-container">
     <div className="create-detail-basic-option">
       <h4 className="bold">Project Title</h4>
       <p className="regular">what is the title of your project?</p>
-      <Input placeholder="Basic usage" id="projectTitle" onChange={onInput} />
+      <Input style={{ maxWidth: '700px' }} placeholder="Basic usage" id="projectTitle" onChange={onInput} />
     </div>
     <div className="create-detail-basic-option">
       <h4 className="bold">Project Tagline</h4>
       <p className="regular">Provide a short description that best describes your project to your audience.</p>
-      <Input placeholder="Basic usage" id="projectTagline" onChange={onInput} />
+      <Input style={{ maxWidth: '700px' }} placeholder="Basic usage" id="projectTagline" onChange={onInput} />
     </div>
     <div className="create-detail-basic-option">
       <h4 className="bold">Project Image</h4>
@@ -77,7 +92,15 @@ export const CreateDetailBasic = ({ onInput, onSelect, imageUrl, onImageUpload, 
     <div className="create-detail-basic-option">
       <h4 className="bold">Tags</h4>
       <p className="regular">Enter up to five keywords that best describe your project.</p>
-      <Select mode="tags" style={{ width: '100%' }} placeholder="Tags Mode" onChange={(change) => onSelect(change, 'tags')} />
+      <Select mode="tags" style={{ width: '100%', maxWidth: '700px' }} placeholder="Tags Mode" onChange={(change) => onSelect(change, 'tags')} />
     </div>
   </div>
 );
+
+CreateDetailBasic.propTypes = {
+  onInput: PropTypes.func,
+  onSelect: PropTypes.func,
+  imageUrl: PropTypes.string,
+  onImageUpload: PropTypes.func,
+  setImage: PropTypes.func,
+}
