d62040fb1b5097f27b44cc8d020d53a70c6a1bb0
PapopCtck <dpprachaya@gmail.com>
2019-12-25 18:48:23 +0700

    [Update] Render Header & Use new function & Move Style to helper folder

--- src/helpers/TabsBar/TabsBar.jsx
+++ src/helpers/TabsBar/TabsBar.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Layout } from 'antd';
+import { Layout, Button, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { MainNav, MainFooter } from '../../component';
 
+import './StyleTabsBar.css';
+
+const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
 export function TabsBar(props) {
   const [activeTab, setActive] = useState(0);
+  const tabLength = props.children.length;
   return (
     <div className="tab-container">
       <ul className="tab-ul">
@@ -23,9 +29,11 @@ export function TabsBar(props) {
         <Content className="tab-content-content">
           {props.children.map((child, index) => {
             const isActiveTab = (activeTab === index);
-            const children = child.props.children;
-
-            return React.createElement(TabContent, { isActiveTab }, children)
+            const { children, header } = child.props;
+            const onNext = () => activeTab + 1 >= tabLength ? null : setActive(activeTab + 1)
+            const onPrev = () => activeTab - 1 < 0 ? null : setActive(activeTab - 1)
+            const { onFinish } = props;
+            return React.createElement(TabContent, { isActiveTab, onNext, onPrev, activeTab, tabLength, onFinish, header }, children)
           })}
         </Content>
         <Footer>
@@ -36,12 +44,56 @@ export function TabsBar(props) {
   );
 }
 
+TabsBar.propTypes = {
+  children: PropTypes.array,
+}
+
 export const Tab = ({ isActiveTab, onTabClick, label }) => (
   <li className={`tab ${isActiveTab ? 'is-active bold' : ''}`} onClick={onTabClick}>
     {label}
   </li>
+);
+
+Tab.propTypes = {
+  isActiveTab: PropTypes.bool, 
+  onTabClick: PropTypes.func, 
+  label: PropTypes.string,
+}
+
+export const TabContent = ({ children, isActiveTab, onNext, onPrev, activeTab, tabLength, onFinish, header }) => (
+  <div className={`tab-content ${isActiveTab ? 'is-active' : ''}`}>
+    <div className="tab-content-header">
+      <Title level={3} className="create-title tab-content-header-title">{header}</Title>
+      <div className="tab-content-header-button-container">
+        <Button className="tab-content-button" type="link"><Link to="/">Save your project</Link></Button>
+        <Button className="tab-content-button" type="primary" ><Link to="/">Publish</Link></Button>
+      </div>
+    </div>
+    <div className="tab-content-child-container">
+      {children}
+      <div className="tab-content-button-container">
+        {
+          activeTab + 1 === tabLength ?
+            <Button className="tab-content-button" type="primary" onClick={onFinish}>Publish</Button>
+            : <Button className="tab-content-button" type="primary" onClick={onNext}>Next</Button>
+        }
+        {
+          activeTab === 0 ?
+            <Button className="tab-content-button" ><Link to="/">Cancel</Link></Button>
+            : <Button className="tab-content-button" onClick={onPrev}>Prev</Button>
+        }
+      </div>
+    </div>
+  </div>
 )
 
-export const TabContent = ({ children, isActiveTab }) => (
-  <div className={`tab-content ${isActiveTab ? 'is-active' : ''}`}>{children}</div>
-)
+TabContent.propTypes = {
+  children:PropTypes.any, 
+  isActiveTab: PropTypes.bool, 
+  onNext: PropTypes.func, 
+  onPrev: PropTypes.func, 
+  activeTab: PropTypes.number, 
+  tabLength: PropTypes.number, 
+  onFinish: PropTypes.func, 
+  header: PropTypes.string,
+}
