495fe060651e5297c1b5a332ad73fa57e1a25a0f
PapopCtck <dpprachaya@gmail.com>
2020-01-29 19:37:23 +0700

    [Update] Add context to appjs and add AppLang useState to appjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
+
+import AppContext from './AppContext';
+
 import {
   Main,
   Login,
@@ -21,31 +24,49 @@ import { MainNav, MainFooter } from './component';
 const { Header, Content, Footer } = Layout;
 
 function App() {
+
+  const useStateWithLocalStorage = localStorageKey => {
+    const [appLang, setLang] = React.useState(
+      localStorage.getItem(localStorageKey) || 'en'
+    );
+    React.useEffect(() => {
+      localStorage.setItem(localStorageKey, appLang);
+    }, [appLang]);
+    return [appLang, setLang];
+  };
+
+  const [appLang, setLang] = useStateWithLocalStorage(
+    'appLang'
+  );
+
+
   return (
     <BrowserRouter>
       <Layout className="layout">
         <Switch>
           <Route sensitive strict exact path="/createproject" component={null} />
           <Header className="header-container">
-            <Route sensitive strict exact component={MainNav} />
+            <Route sensitive strict exact render={() => <MainNav appLang={appLang} setLang={setLang} />} />
           </Header>
         </Switch>
         <Layout>
-          <Content >
-            <Switch>
-              <Route sensitive strict exact path="/" component={Main} />
-              <Route sensitive strict path="/login" component={Login} />
-              <Route sensitive strict path="/register" component={Register} />
-              <Route sensitive strict path="/project/:id" component={ProjectDetail} />
-              <Route sensitive strict path="/explore" component={Explore} />
-              <Route sensitive strict path="/profile" component={Profile} />
-              <Route sensitive strict path="/createproject" component={CreateProject} />
-              <Route sensitive strict path="/trackproject" component={TrackProject} />
-              <Route sensitive strict path="/403" component={Error403} />
-              <Route sensitive strict path="/500" component={Error500} />
-              <Route sensitive strict component={Error404} />
-            </Switch>
-          </Content>
+          <AppContext.Provider value={appLang}>
+            <Content >
+              <Switch>
+                <Route sensitive strict exact path="/" component={Main} />
+                <Route sensitive strict path="/login" component={Login} />
+                <Route sensitive strict path="/register" component={Register} />
+                <Route sensitive strict path="/project/:id" component={ProjectDetail} />
+                <Route sensitive strict path="/explore" component={Explore} />
+                <Route sensitive strict path="/profile" component={Profile} />
+                <Route sensitive strict path="/createproject" component={CreateProject} />
+                <Route sensitive strict path="/trackproject" component={TrackProject} />
+                <Route sensitive strict path="/403" component={Error403} />
+                <Route sensitive strict path="/500" component={Error500} />
+                <Route sensitive strict component={Error404} />
+              </Switch>
+            </Content>
+          </AppContext.Provider>
         </Layout>
         <Switch>
           <Route sensitive strict exact path="/createproject" component={null} />
