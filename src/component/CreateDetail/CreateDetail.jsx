0f18e17a8309c67c020ede8f44b729beeca613db
PapopCtck <dpprachaya@gmail.com>
2020-02-10 18:27:18 +0700

    [Update] Update localization for createdetail component

--- src/component/CreateDetail/CreateDetail.jsx
+++ src/component/CreateDetail/CreateDetail.jsx
@@ -12,6 +12,10 @@ import {
   UserSearch,
 } from '../../helpers';
 
+import AppLang from '../../AppContext';
+
+import content from './LangCreateDetail';
+
 import 'react-quill/dist/quill.snow.css';
 
 import './StyleCreateDetail.css';
@@ -21,44 +25,41 @@ const { Title } = Typography;
 export class CreateDetail extends Component {
   render() {
     const { show, onInput, handleChange, imageUrl, onFinish, projectStory, setLang } = this.props;
+    const appLang = this.context;
     return (
       <div className="create-detail-container">
         <QueueAnim className="create-basic" delay={650} type={['bottom', 'top']} ease={['easeOutQuart', 'easeInOutQuart']}>
           {!show ? [
             <QueueAnim key="page" type="bottom">
-              <TabsBar onFinish={onFinish} setLang={setLang} >
-                <Tab label="Basic" header="Basic information" >
-                  <Title level={4} className="create-title">Basics</Title>
-                  <p className="create-description">
-                    Make a good first impression: introduce your project objectives and entice people to learn more.
-                      This basic information will represent your project on your project page, on your project card,
-                      and in searches.</p>
+              <TabsBar onFinish={onFinish} setLang={setLang} content={content[appLang]} >
+                <Tab label={content[appLang].basic.title} header={content[appLang].basic.header} >
+                  <Title level={4} className="create-title">{content[appLang].basic.title}</Title>
+                  <p className="create-description">{content[appLang].basic.description}</p>
                   <CreateDetailBasic
                     onInput={onInput}
                     handleChange={handleChange}
                     imageUrl={imageUrl}
+                    content={content[appLang].basic}
                   />
                 </Tab>
-                <Tab label="Story" header="Project Story">
-                  <Title level={4} className="create-title">Story</Title>
-                  <p className="create-description">
-                    Tell people why they should be excited about your project. Get specific but be clear and be brief..</p>
-                  <CreateStory handleChange={handleChange} value={projectStory} />
+                <Tab label={content[appLang].story.title} header={content[appLang].story.header}>
+                  <Title level={4} className="create-title">{content[appLang].story.title}</Title>
+                  <p className="create-description">{content[appLang].story.description}</p>
+                  <CreateStory handleChange={handleChange} value={projectStory} content={content[appLang].story} />
                 </Tab>
-                <Tab label="FAQ" header="Frequently Ask Question">
-                  <Title level={4} className="create-title">FAQ</Title>
-                  <p className="create-description">
-                    The FAQ section should provide the most common details that peoples are looking for when evaluating your project.</p>
-                  <DynamicForm onChange={handleChange} />
+                <Tab label={content[appLang].faq.title} header={content[appLang].faq.header}>
+                  <Title level={4} className="create-title">{content[appLang].faq.title}</Title>
+                  <p className="create-description">{content[appLang].faq.description}</p>
+                  <DynamicForm onChange={handleChange} content={content[appLang].faq} />
                 </Tab>
-                <Tab label="People" header="Add People">
-                  <Title level={4} className="create-title">Contributor</Title>
-                  <p className="create-description">
-                    Add people who are working for this project or contribute something to the project.</p>
+                <Tab label={content[appLang].contributor.title} header={content[appLang].contributor.header}>
+                  <Title level={4} className="create-title">{content[appLang].contributor.title}</Title>
+                  <p className="create-description">{content[appLang].contributor.description}</p>
                   <UserSearch style={{ maxWidth: '700px' }} />
                 </Tab>
-                <Tab label="Extras" header="Extras">
-                  test5
+                <Tab label={content[appLang].people.title} header={content[appLang].people.header}>
+                  <Title level={4} className="create-title">{content[appLang].people.title}</Title>
+                  <p className="create-description">{content[appLang].people.description}</p>
                 </Tab>
               </TabsBar>
             </QueueAnim>,
@@ -69,6 +70,8 @@ export class CreateDetail extends Component {
     )
   }
 }
+
+CreateDetail.contextType = AppLang;
 
 export default CreateDetail;
 
@@ -85,27 +88,27 @@ CreateDetail.propTypes = {
   projectStory: PropTypes.string,
 }
 
-export const CreateDetailBasic = ({ onInput, handleChange, imageUrl }) => (
+export const CreateDetailBasic = ({ onInput, handleChange, imageUrl, content }) => (
   <div className="create-detail-basic-container">
     <div className="create-detail-basic-option">
-      <h4 className="bold">Project Title</h4>
-      <p className="regular">what is the title of your project?</p>
-      <Input style={{ maxWidth: '700px' }} placeholder="Basic usage" id="projectTitle" onChange={onInput} />
+      <h4 className="bold">{content.projectTitle}</h4>
+      <p className="regular">{content.projectTitleDescription}</p>
+      <Input style={{ maxWidth: '700px' }} placeholder={content.projectTitle} id="projectTitle" onChange={onInput} />
     </div>
     <div className="create-detail-basic-option">
-      <h4 className="bold">Project Tagline</h4>
-      <p className="regular">Provide a short description that best describes your project to your audience.</p>
-      <Input style={{ maxWidth: '700px' }} placeholder="Basic usage" id="projectDescription" onChange={onInput} />
+      <h4 className="bold">{content.projectTagline}</h4>
+      <p className="regular">{content.projectTaglineDescription}</p>
+      <Input style={{ maxWidth: '700px' }} placeholder={content.projectTagline} id="projectDescription" onChange={onInput} />
     </div>
     <div className="create-detail-basic-option">
-      <h4 className="bold">Project Image</h4>
-      <p className="regular">Upload a square image that represents your project.</p>
+      <h4 className="bold">{content.projectImage}</h4>
+      <p className="regular">{content.projectImageDescription}</p>
       <ImageUploader imageUrl={imageUrl} handleChange={handleChange} />
     </div>
     <div className="create-detail-basic-option">
-      <h4 className="bold">Tags</h4>
-      <p className="regular">Enter up to five keywords that best describe your project.</p>
-      <Select mode="tags" style={{ width: '100%', maxWidth: '700px' }} placeholder="Tags Mode" onChange={(change) => handleChange(change, 'tags')} />
+      <h4 className="bold">{content.projectTags}</h4>
+      <p className="regular">{content.projectTagsDescription}</p>
+      <Select mode="tags" style={{ width: '100%', maxWidth: '700px' }} placeholder={content.projectTags} onChange={(change) => handleChange(change, 'tags')} />
     </div>
   </div>
 );
@@ -117,6 +120,7 @@ CreateDetailBasic.propTypes = {
   onImageUpload: PropTypes.func,
   setImage: PropTypes.func,
   handleChange: PropTypes.func,
+  content: PropTypes.object,
 }
 
 const modules = {
@@ -140,10 +144,10 @@ const formats = [
   'link', 'image',
 ]
 
-export const CreateStory = ({ handleChange, value }) => (
+export const CreateStory = ({ handleChange, value, content }) => (
   <div className="create-detail-story-container">
-    <h4 className="bold">Project Description</h4>
-    <p className="regular">Describe what you planning to do, why are you doing it, how you plan to make it happen, and who you are.</p>
+    <h4 className="bold">{content.projectDesc}</h4>
+    <p className="regular">{content.projectDescDescription}</p>
     <ReactQuill
       className="create-detail-story-editor"
       placeholder="Write a story of your project. Don't be shy."
@@ -160,4 +164,5 @@ CreateStory.propTypes = {
   onInput: PropTypes.func,
   value: PropTypes.string,
   handleChange: PropTypes.func,
+  content: PropTypes.object,
 }
