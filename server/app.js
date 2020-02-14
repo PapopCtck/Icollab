d3ecc9983b0fefce930db8cd9527501783523241
PapopCtck <dpprachaya@gmail.com>
2020-02-14 20:03:21 +0700

    [Update] Update server encoding

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 // server/app.js
 const express = require('express');
 const path = require('path');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 
 // Server static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(expressStaticGzip(path.resolve(__dirname, '..', 'build'), {
+  enableBrotli: true,
+  customCompressions: [{
+    encodingName: 'deflate',
+    fileExtension: 'zz',
+  }],
+  orderPreference: ['br', 'gz'],
+}));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
