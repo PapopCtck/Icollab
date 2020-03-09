cbe935ae360a237b9069743d1867167af687283b
PapopCtck <dpprachaya@gmail.com>
2020-03-09 20:40:02 +0700

    [Update] Add apply modal in ProjectDetailHeader

--- src/component/ProjectDetailHeader/ProjectDetailHeader.jsx
+++ src/component/ProjectDetailHeader/ProjectDetailHeader.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
-import { Typography, Button, Icon, Modal, Select, Input, Form, notification } from 'antd';
-import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import {
+  Typography,
+  Button,
+  Icon,
+  Modal,
+  Select,
+  Input,
+  Form,
+  notification,
+  message,
+  Avatar,
+  Rate,
+} from 'antd';
+import PropTypes from 'prop-types';
+
+import { getCookie } from '../../helpers';
+
+import { fetchGetProfile } from '../../actions';
 
 import './StyleProjectDetailHeader.css';
 
@@ -22,41 +40,89 @@ const openNotification = theme => {
   });
 };
 
+const error = () => {
+  message.error('Please login before continue!');
+};
+
 export class DetailHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false,
-      loading: false,
+      showReport: false,
+      reportLoading: false,
+      showApply: false,
+      user: null,
+      applyLoading: false,
     };
+    if (getCookie('icollab_userinfo')) {
+      const id = JSON.parse(getCookie('icollab_userinfo'))[0].user_uid;
+      console.log(id)
+      props.dispatch(fetchGetProfile({ id }));
+    }
   }
 
-  toggleModal = () => {
-    const { showModal } = this.state;
+  componentDidUpdate(prevProps) {
+    if (prevProps.fetchGetProfile !== this.props.fetchGetProfile) {
+      const fetchGetProfile = this.props.fetchGetProfile;
+      this.setState({ user: fetchGetProfile.User[0] }, () => console.log(this.state));
+    }
+  }
+
+  toggleReport = () => {
+    const { showReport } = this.state;
     this.setState({
-      showModal: !showModal,
+      showReport: !showReport,
     });
   };
 
+  toggleApply = () => {
+    const { showApply } = this.state;
+    if (getCookie('icollab_token')) {
+      this.setState({
+        showApply: !showApply,
+      });
+    } else {
+      error();
+    }
+  };
 
-  handleSubmit = (e, validateFieldsAndScroll, theme) => {
-    this.setState({ loading: true })
+  handleReportSubmit = (e, validateFieldsAndScroll, resetFields, theme) => {
+    this.setState({ reportLoading: true })
     e.preventDefault();
     validateFieldsAndScroll((err, reportForm) => {
       if (!err) {
         console.log('Received values of form: ', reportForm);
-        this.toggleModal();
-        this.setState({ loading: false });
+        this.toggleReport();
+        this.setState({ reportLoading: false });
         openNotification(theme);
-        this.props.form.resetFields();
+        resetFields();
       } else {
-        this.setState({ loading: false })
+        this.setState({ reportLoading: false })
+      }
+    });
+  };
+
+  handleApplySubmit = (e, validateFieldsAndScroll, resetFields) => {
+    this.setState({ applyLoading: true })
+    e.preventDefault();
+    validateFieldsAndScroll((err, applyForm) => {
+      if (!err) {
+        console.log('Received values of form: ', applyForm);
+        setTimeout(() => {
+          this.setState({ applyLoading: false });
+          this.toggleApply();
+          resetFields();
+          this.props.history.push('/success')
+        }, 1000)
+
+      } else {
+        this.setState({ applyLoading: false })
       }
     });
   };
 
   render() {
-    const { showModal, loading } = this.state;
+    const { showReport, reportLoading, showApply, user, applyLoading } = this.state;
     const { projectDetail, theme } = this.props;
     return (
       <div className="detailheader-container">
@@ -64,7 +130,8 @@ export class DetailHeader extends Component {
           <img className="detailheader-image" src={projectDetail.image} alt={projectDetail.image} />
         </div>
         <div className="detailheader-content-container">
-          <WrappedReportForm showModal={showModal} loading={loading} theme={theme} toggleModal={this.toggleModal} handleSubmit={this.handleSubmit} />
+          <WrappedReportForm showReport={showReport} loading={reportLoading} theme={theme} toggleReport={this.toggleReport} handleSubmit={this.handleReportSubmit} />
+          <WrappedApplyForm showApply={showApply} toggleApply={this.toggleApply} theme={theme} user={user} handleSubmit={this.handleApplySubmit} loading={applyLoading} />
           <div className="detailheader-content-title">
             <Title className={theme + '-text'} level={2}>
               {projectDetail.projecttitle}
@@ -90,7 +157,7 @@ export class DetailHeader extends Component {
               <span className="bold">location : </span>
               {projectDetail.location}
             </div>
-            <Button className="detailheader-applybtn" type="primary" block>
+            <Button className="detailheader-applybtn" type="primary" block onClick={this.toggleApply}>
               Apply now
             </Button>
             <div className="detailheader-carousel-contact-container">
@@ -101,7 +168,7 @@ export class DetailHeader extends Component {
               <Icon type="reddit" className="primary-icon social-icon" />
             </div>
             <div className="detailheader-carousel-report-container">
-              think this project should be at another level ? <Button className="detailheader-report-button bold" onClick={this.toggleModal} type="link">tell us</Button>
+              think this project should be at another level ? <Button className="detailheader-report-button bold" onClick={this.toggleReport} type="link">tell us</Button>
             </div>
           </div>
         </div>
@@ -110,7 +177,13 @@ export class DetailHeader extends Component {
   }
 }
 
-export default DetailHeader
+const mapStateToProps = state => {
+  const fetchGetProfile = state.fetchGetProfile.data;
+  return { fetchGetProfile };
+}
+
+export default withRouter(connect(mapStateToProps)(DetailHeader));
+
 
 DetailHeader.propTypes = {
   projectDetail: PropTypes.object,
@@ -118,18 +191,18 @@ DetailHeader.propTypes = {
 };
 
 
-const ReportForm = ({ form: { getFieldDecorator, validateFieldsAndScroll }, showModal, loading, theme, toggleModal, handleSubmit }) => (
+const ReportModal = ({ form: { getFieldDecorator, validateFieldsAndScroll, resetFields }, showReport, loading, theme, toggleReport, handleSubmit }) => (
   <Modal
     className="detailheader-report-modal"
-    visible={showModal}
-    onCancel={toggleModal}
+    visible={showReport}
+    onCancel={toggleReport}
     width={400}
     footer={null}
     bodyStyle={theme == 'dark' ? { background: '#29292e' } : { background: 'white' }}
     centered
   >
     <Form className="report-form"
-      onSubmit={e => handleSubmit(e, validateFieldsAndScroll ,theme)}
+      onSubmit={e => handleSubmit(e, validateFieldsAndScroll, resetFields, theme)}
     >
       <Title className={theme + '-text'} level={2}>Report</Title>
       <h3 className={theme + '-text'}>Choose a reason</h3>
@@ -174,4 +247,59 @@ const ReportForm = ({ form: { getFieldDecorator, validateFieldsAndScroll }, show
   </Modal>
 );
 
-export const WrappedReportForm = Form.create({ name: 'report' })(ReportForm);
+export const WrappedReportForm = Form.create({ name: 'report' })(ReportModal);
+
+const ApplyModal = ({ form: { getFieldDecorator, validateFieldsAndScroll, resetFields }, showApply, toggleApply, theme, user, handleSubmit, loading }) => {
+  if (user) {
+    return (
+      <Modal
+        visible={showApply}
+        width={350}
+        footer={null}
+        onCancel={toggleApply}
+        bodyStyle={theme == 'dark' ? { background: '#29292e' } : { background: 'white' }}
+        centered
+      >
+        <div className="applyModal-container">
+          <Avatar className="applyModal-avatar" size={128} icon="user" />
+          <Rate className="applyModal-rate" style={{ marginTop: '20px' }} disabled defaultValue={0} />
+          <h2 className={'applyModal-username ' + theme + '-text'}>{user.name + ' ' + user.lastname}</h2>
+          <h3 className={theme + '-text'}>Applied for</h3>
+          <Form className="apply-form"
+            onSubmit={e => handleSubmit(e, validateFieldsAndScroll, resetFields)}
+          >
+            <Form.Item>
+              {getFieldDecorator('position', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please specified your position for this application!',
+                  },
+                ],
+              })(
+                <Select
+                  style={{ width: '100%', marginBottom: '10px' }}
+                  placeholder="Select a reason"
+                >
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="disabled" disabled>
+                    Disabled
+                  </Option>
+                  <Option value="Yiminghe">yiminghe</Option>
+                </Select>,
+              )}
+            </Form.Item>
+            <p className={theme + '-text'}>by clicking apply you agree to our Term of Service and Privacy Policy</p>
+            <Button htmlType="submit" type="primary" block loading={loading}>
+              Apply
+            </Button>
+          </Form>
+        </div>
+      </Modal>
+    )
+  }
+  return null;
+}
+
+export const WrappedApplyForm = Form.create({ name: 'apply' })(ApplyModal);
